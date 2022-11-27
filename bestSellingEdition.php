7f1b0d8ea3f7b2dfba7f9f74d637dd291502aee6
Akanksha <42142815+akankshapawar1@users.noreply.github.com>
2022-11-27 18:23:59 -0500

    Added Query Class 4
    Need to refine Query 4 views

---
+++ bestSellingEdition.php
@@ -0,0 +1,182 @@
+<!-- PHP code to establish connection with the localserver -->
+<?php
+
+ 
+// Username is root
+$user = 'admin';
+$password = 'Fit4M0Re!';
+ 
+// Database name is geeksforgeeks
+$database = 'BookStore';
+// Server is localhost with
+// port number 3306
+$servername='dbms-project.csddeoelb5pk.us-east-1.rds.amazonaws.com:3306';
+$mysqli = new mysqli($servername, $user,
+                $password, $database);
+$link = mysqli_connect($servername, $user, $password, $database);
+
+
+// Checking for connections
+if ($mysqli->connect_error) {
+    die('Connect Error (' .
+    $mysqli->connect_errno . ') '.
+    $mysqli->connect_error);
+}
+
+// SQL query to select data from database
+
+
+$sql = "select author_writes_books.BookID, BooksWithMultipleEditionsAndISBNSold.isbn,                       author_writes_books.Author_name, author_writes_books.Book, BooksWithMultipleEditionsAndISBNSold.NumberOfCopiesSold, RATING_VIEWS.Rating, 
+    Edition.price, Edition.Format 
+from BooksWithMultipleEditionsAndISBNSold
+INNER JOIN author_writes_books ON author_writes_books.BookID = BooksWithMultipleEditionsAndISBNSold.bid
+INNER JOIN RATING_VIEWS ON RATING_VIEWS.bid = author_writes_books.BookID
+INNER JOIN Info ON Info.bid = author_writes_books.BookID
+INNER JOIN Edition ON Edition.bid = author_writes_books.BookID 
+group by BooksWithMultipleEditionsAndISBNSold.title;
+";
+
+$result = $mysqli->query($sql);
+#$mysqli->close();
+
+
+
+
+if(isset($_POST['add'])){
+
+                $_SESSION['title']=$_POST['title'];
+                $_SESSION['price']=$_POST['price'];
+                $_SESSION['quantity']=$_POST['quantity'];
+                $_SESSION['bid']=$_POST['bid'];
+                $_SESSION['Format']=$_POST['Format'];
+                $_SESSION['Rating']=$_POST['Rating'];
+
+                $sql = "CALL getPopularBookByGenre('Young Adult');";
+                
+                $sql2="
+                INSERT INTO Cart(email,quantity,price,book_title,bid,Format,Rating)
+                Values('aallen@example.net','".$_SESSION['quantity']."','".$_SESSION['price']."','".$_SESSION['title']."','".$_SESSION['bid']."','".$_SESSION['Format']."','".$_SESSION['Rating']."')";
+
+                if(mysqli_query($link, $sql2)){
+                
+                } else{
+                echo "ERROR: Could not able to execute $sql2. " . mysqli_error($link);
+                }                 
+                
+                $result = $mysqli->query($sql);
+                $mysqli->close();
+                }
+?>
+
+<!-- HTML code to display data in tabular format -->
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Online Book Store</title>
+    
+    <!-- CSS FOR STYLING THE PAGE -->
+    <style>
+        table {
+            margin: 0 auto;
+            font-size: large;
+            border: 1px solid black;
+        }
+ 
+        h1 {
+            text-align: center;
+            color: #006600;
+            font-size: xx-large;
+            font-family: 'Gill Sans', 'Gill Sans MT',
+            ' Calibri', 'Trebuchet MS', 'sans-serif';
+        }
+ 
+        td {
+            background-color: #E4F5D4;
+            border: 1px solid black;
+        }
+ 
+        th,
+        td {
+            font-weight: bold;
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+        }
+ 
+        td {
+            font-weight: lighter;
+        }
+    </style>
+</head>
+ 
+<body>
+    <form action="cart.php" method="post" accept-charset="utf-8" class="custom-add2cart">
+   <div class="add-button-wrapper widget-fingerprint-product-add-button">
+       <input type="submit" name="display_cart"class="btn regular-button regular-main-button add2cart submit" value="Display cart">   
+   </div>
+</form>
+    <section>
+        <h1>Best Selling Editions</h1>
+        <!-- TABLE CONSTRUCTION author_writes_books.BookID, BooksWithMultipleEditionsAndISBNSold.isbn,                       author_writes_books.Author_name, author_writes_books.Book, BooksWithMultipleEditionsAndISBNSold.NumberOfCopiesSold, RATING_VIEWS.Rating, Edition.price, Edition.Format 
+                -->
+        <table>
+            <tr>
+                <th>Book ID</th>
+                <th>ISBN</th>
+                <th>Book</th>
+                <th>Author</th>
+                <th>Number Of Copies Sold</th>
+                <th>Price</th>
+                <th>Format</th>
+                <th>Add to Cart</th>
+                <th>Ratings</th>
+                
+            </tr>
+            <!-- PHP CODE TO FETCH DATA FROM ROWS -->
+            <?php
+
+                // LOOP TILL END OF DATA
+                while($rows=$result->fetch_assoc())
+                {
+                    $_SESSION["BookTitle"]=$rows['Book'];
+                    $_SESSION["Price"]=$rows['price'];
+                    $_SESSION["Rating"]=$rows['Rating'];
+                    
+            ?>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+            <tr>
+                <!-- FETCHING DATA FROM EACH
+                    ROW OF EVERY COLUMN -->
+                <td><input type="text" name="bid" value="<?php echo $rows['BookID'];?>" readonly></td>
+                <td><?php echo $rows['isbn'];?></td>
+                <td><input type="text" name="title" value="<?php echo $rows["Book"] ?>" readonly></td>
+                <td><?php echo $rows['Author_name'];?></td>
+                <td><?php echo $rows['NumberOfCopiesSold'];?></td>
+                <td><input type="text" name="Format" value="<?php echo $rows['Format'];?>" readonly></td>
+                <td><input type="text" name="price" value="<?php echo $rows['price'] ?>" readonly></td>
+                <td><input type="number" name="quantity" min="1" max="100"><input type="submit" name="add" value="Add to Cart"></td>
+                <td><input type="text" name="Rating" value="<?php echo round($rows['Rating'],2);?>" readonly>
+            <!--</tr>-->
+        </form>
+        <form action="rating.php" method="POST">
+                <input type="text" name="bid" value="<?php echo $rows['BookID'];?>" hidden>
+
+                <input type="submit" name="add1" value="Submit a Review"></td>
+        </form>
+                
+        </tr>
+        </form>
+            <?php
+                
+                }
+            ?>
+        </table>
+        </section>
+
+    
+</body>
+ 
+</html>
